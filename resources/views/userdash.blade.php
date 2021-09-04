055dc955c2b41a434b714ea84452ef4c1afb8b86
Jeanilyn Mendoza <cpjeanilynmendoza@gmail.com>
2021-09-04 12:32:45 +0800

    Modified

--- resources/views/userdash.blade.php
+++ resources/views/userdash.blade.php
@@ -6,19 +6,34 @@
                 <div class="p-6 bg-white border-b border-gray-200 d-flex">
                     <div class="row">
                         <div class="col-sm-6">
-                            <img src="/img/logo.png" style="width:100%" alt="">
+                            <img src="/img/logo.png" style="width:100%" class="w-100 rounded-circle">
                         </div>
                         <div class="col-sm-6">
-                            <div><h1>Your Blog</h1></div>
+                            <div class="d-flex align-items-center pb-4"><h1>Your Blog</h1></div>
                             <div class="d-flex">
                                 <div> <strong>1.5k</strong> Blogs</div>
                                 <div><strong>500</strong> Followers</div>
                                 <div><strong>400</strong> Following</div>
                             </div>
+
+                            <div class="pt-4 font-wieght-bold">Your Blog</div>
+                            <div>This is the sample description</div>
+                            <div><a href="#" style="color: #17a2b8;">YourBLog.com</a></div>
                         </div>
-                    </div>
+                     </div>  <!-- endDiv for row1 -->
                 </div>
             </div>
+            <div class="row mt-3" style="border-radius:5px;">
+                        <div class="col-4">
+                        <img src="/img/logo.png" class="w-100">
+                        </div>
+                        <div class="col-4">
+                        <img src="/img/logo.png" class="w-100">
+                        </div>
+                        <div class="col-4">
+                        <img src="/img/logo.png" class="w-100">
+                        </div>
+                    </div>  <!-- endDiv for row2 -->
         </div>
     </div>
 </x-app-layout>
