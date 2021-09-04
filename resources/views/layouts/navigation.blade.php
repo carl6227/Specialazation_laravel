167d14b49b9e3b90d51dd457e37d3953c865dae5
carl6227 <19104922@usc.edu.ph>
2021-09-04 08:08:05 +0800

    first update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,12 +6,13 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                      
+                        <img src="/img/logo.png" style="width:100px;" alt="">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px  sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Your Blog') }}
                     </x-nav-link>
--- resources/views/userdash.blade.php
+++ resources/views/userdash.blade.php
@@ -1,15 +1,22 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard For Users') }}
-        </h2>
-    </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in as Users!
+                <div class="p-6 bg-white border-b border-gray-200 d-flex">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <img src="/img/logo.png" style="width:100%" alt="">
+                        </div>
+                        <div class="col-sm-6">
+                            <div><h1>Your Blog</h1></div>
+                            <div class="d-flex">
+                                <div> <strong>1.5k</strong> Blogs</div>
+                                <div><strong>500</strong> Followers</div>
+                                <div><strong>400</strong> Following</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
